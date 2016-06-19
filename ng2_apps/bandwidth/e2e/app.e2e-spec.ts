94233effee8f494e160899e773fd46445c6951f8
Mike Morano <mmorano@mikeandwan.us>
2016-06-19 08:51:17 -0400

    upgrade to cli.beta6

---
+++ ng2_apps/bandwidth/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BandwidthPage } from './app.po';
+
+describe('bandwidth App', function() {
+  let page: BandwidthPage;
+
+  beforeEach(() => {
+    page = new BandwidthPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

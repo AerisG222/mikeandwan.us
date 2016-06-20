4d2a5cac7bda6d982c916359ed510c1ead1b4cb4
Mike Morano <mmorano@mikeandwan.us>
2016-06-19 20:10:30 -0400

    upgrade to angular-cli.beta6

--- ng2_apps/photos/src/system-config.ts
+++ ng2_apps/photos/src/system-config.ts
@@ -1,3 +1,7 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+
 /***********************************************************************************************
  * User Configuration.
  **********************************************************************************************/
@@ -14,44 +18,27 @@ const packages: any = {
  * Everything underneath this line is managed by the CLI.
  **********************************************************************************************/
 const barrels: string[] = [
-    // Angular specific barrels.
-    '@angular/core',
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/http',
-    '@angular/router',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
+  // Angular specific barrels.
+  '@angular/core',
+  '@angular/common',
+  '@angular/compiler',
+  '@angular/http',
+  '@angular/router',
+  '@angular/platform-browser',
+  '@angular/platform-browser-dynamic',
 
-    // Thirdparty barrels.
-    'rxjs',
+  // Thirdparty barrels.
+  'rxjs',
 
-    // App specific barrels.
-    'app',
-    'app/shared',
-    'app/category-link',
-    'app/category-list',
-    'app/comment-view',
-    'app/effects-view',
-    'app/exif-view',
-    'app/fullscreen-view',
-    'app/header',
-    'app/help-dialog',
-    'app/map-view',
-    'app/mode',
-    'app/photo-dialog',
-    'app/photo-list',
-    'app/photo-view',
-    'app/preference-dialog',
-    'app/rating-view',
-    'app/save-dialog',
-    'app/slideshow-button',
-    /** @cli-barrel */
+  // App specific barrels.
+  'app',
+  'app/shared',
+  /** @cli-barrel */
 ];
 
 const cliSystemConfigPackages: any = {};
 barrels.forEach((barrelName: string) => {
-    cliSystemConfigPackages[barrelName] = { main: 'index' };
+  cliSystemConfigPackages[barrelName] = { main: 'index' };
 });
 
 /** Type declaration for ambient System. */
@@ -59,12 +46,12 @@ declare var System: any;
 
 // Apply the CLI SystemJS configuration.
 System.config({
-    map: {
-        '@angular': 'vendor/@angular',
-        'rxjs': 'vendor/rxjs',
-        'main': 'main.js'
-    },
-    packages: cliSystemConfigPackages
+  map: {
+    '@angular': 'vendor/@angular',
+    'rxjs': 'vendor/rxjs',
+    'main': 'main.js'
+  },
+  packages: cliSystemConfigPackages
 });
 
 // Apply the user's configuration.

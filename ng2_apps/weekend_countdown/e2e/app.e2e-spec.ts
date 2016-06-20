0f3de7f8688f3a65bc9560ec6a63873c99eed952
Mike Morano <mmorano@mikeandwan.us>
2016-06-19 20:12:12 -0400

    upgrade to angular-cli.beta6

---
+++ ng2_apps/weekend_countdown/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { WeekendCountdownPage } from './app.po';
+
+describe('weekend-countdown App', function() {
+  let page: WeekendCountdownPage;
+
+  beforeEach(() => {
+    page = new WeekendCountdownPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

f4cd8a9afb5b41ae8bf69cf300cf4449b5394802
Mike Morano <mmorano@mikeandwan.us>
2016-06-19 20:07:20 -0400

    update to angular-cli.beta6

--- ng2_apps/byte_counter/e2e/app.po.ts
+++ ng2_apps/byte_counter/e2e/app.po.ts
@@ -1,9 +1,9 @@
 export class ByteCounterPage {
-    navigateTo() {
-        return browser.get('/');
-    }
+  navigateTo() {
+    return browser.get('/');
+  }
 
-    getParagraphText() {
-        return element(by.css('byte-counter-app h1')).getText();
-    }
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
 }

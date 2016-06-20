872894a24002a5e75ca806a7d797bd4f7365316f
Mike Morano <mmorano@mikeandwan.us>
2016-06-19 20:09:34 -0400

    upgrade to angular-cli.beta6

--- ng2_apps/memory/e2e/app.po.ts
+++ ng2_apps/memory/e2e/app.po.ts
@@ -1,9 +1,9 @@
 export class MemoryPage {
-    navigateTo() {
-        return browser.get('/');
-    }
+  navigateTo() {
+    return browser.get('/');
+  }
 
-    getParagraphText() {
-        return element(by.css('memory-app h1')).getText();
-    }
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
 }

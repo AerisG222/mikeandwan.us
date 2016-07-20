4ecafdee4cbf4135c765c66532d1a02f48c7799e
Mike Morano <mmorano@mikeandwan.us>
2016-07-20 19:41:28 -0400

    update to include the new exif details

--- client_apps/photos/src/app/exif-view/exif-view.component.ts
+++ client_apps/photos/src/app/exif-view/exif-view.component.ts
@@ -16,53 +16,79 @@ import { Photo } from '../shared/photo.model';
 export class ExifViewComponent {
     private _photo: Photo;
     private _map: Array<ExifFormatter> = [
-        new ExifFormatter('AF Point', 'afPoint', null),
-        new ExifFormatter('Aperture', 'aperture', null),
+        new ExifFormatter('Bits per Sample', 'bitsPerSample', null),
+        new ExifFormatter('Compression', 'compression', null),
         new ExifFormatter('Contrast', 'contrast', null),
-        new ExifFormatter('Depth Of Field', 'depthOfField', null),
+        new ExifFormatter('Create Date', 'createDate', null),
         new ExifFormatter('Digital Zoom Ratio', 'digitalZoomRatio', null),
         new ExifFormatter('Exposure Compensation', 'exposureCompensation', null),
-        new ExifFormatter('Exposure Difference', 'exposureDifference', null),
         new ExifFormatter('Exposure Mode', 'exposureMode', null),
-        new ExifFormatter('Exposure Time', 'exposureTime', this.inverseTime),
+        new ExifFormatter('Exposure Program', 'exposureProgram', null),
+        new ExifFormatter('Exposure Time', 'exposureTime', null),
         new ExifFormatter('F Number', 'fNumber', null),
         new ExifFormatter('Flash', 'flash', null),
-        new ExifFormatter('Flash Exposure Compensation', 'flashExposureCompensation', null),
-        new ExifFormatter('Flash Mode', 'flashMode', null),
-        new ExifFormatter('Flash Setting', 'flashSetting', null),
-        new ExifFormatter('Flash Type', 'flashType', null),
-        new ExifFormatter('Focal Length', 'focalLength', this.focalLengthOneDecimal),
-        new ExifFormatter('Focal Length in 35mm Format', 'focalLengthIn35mmFormat', this.focalLengthNoDecimals),
-        new ExifFormatter('Focus Distance', 'focusDistance', this.distance),
-        new ExifFormatter('Focus Mode', 'focusMode', null),
-        new ExifFormatter('Focus Position', 'focusPosition', null),
+        new ExifFormatter('Focal Length', 'focalLength', null),
+        new ExifFormatter('Focal Length in 35mm Format', 'focalLengthIn35mmFormat', null),
         new ExifFormatter('Gain Control', 'gainControl', null),
-        new ExifFormatter('Hue Adjustment', 'hueAdjustment', null),
-        new ExifFormatter('Hyperfocal Distance', 'hyperFocalDistance', this.distance),
+        new ExifFormatter('GPS Altitude', 'gpsAltitude', this.formatAltitude),
+        new ExifFormatter('GPS Time Stamp', 'gpsDateStamp', null),
+        new ExifFormatter('GPS Direction', 'gpsDirection', null),
+        new ExifFormatter('GPS Latitude', 'gpsLatitude', this.formatLatitude),
+        new ExifFormatter('GPS Longitude', 'gpsLongitude', this.formatLongitude),
+        new ExifFormatter('GPS Measure Mode', 'gpsMeasureMode', null),
+        new ExifFormatter('GPS Satellites', 'gpsSatellites', null),
+        new ExifFormatter('GPS Status', 'gpsStatus', null),
+        new ExifFormatter('GPS Version ID', 'gpsVersionId', null),
         new ExifFormatter('ISO', 'iso', null),
-        new ExifFormatter('Lens ID', 'lensId', null),
         new ExifFormatter('Light Source', 'lightSource', null),
         new ExifFormatter('Make', 'make', null),
         new ExifFormatter('Metering Mode', 'meteringMode', null),
         new ExifFormatter('Model', 'model', null),
-        new ExifFormatter('Noise Reduction', 'noiseReduction', null),
         new ExifFormatter('Orientation', 'orientation', null),
         new ExifFormatter('Saturation', 'saturation', null),
-        new ExifFormatter('Scale Factor 35 EFL', 'scaleFactor35Efl', this.oneDecimal),
         new ExifFormatter('Scene Capture Type', 'sceneCaptureType', null),
         new ExifFormatter('Scene Type', 'sceneType', null),
         new ExifFormatter('Sensing Method', 'sensingMethod', null),
         new ExifFormatter('Sharpness', 'sharpness', null),
-        new ExifFormatter('Shutter Speed', 'shutterSpeed', this.inverseTime),
+        new ExifFormatter('Auto Focus Area Mode', 'autoFocusAreaMode', null),
+        new ExifFormatter('Auto Focus Point', 'autoFocusPoint', null),
+        new ExifFormatter('Active D Lighting', 'activeDLighting', null),
+        new ExifFormatter('Colorspace', 'colorspace', null),
+        new ExifFormatter('Exposure Difference', 'exposureDifference', null),
+        new ExifFormatter('Flash Color Filter', 'flashColorFilter', null),
+        new ExifFormatter('Flash Compensation', 'flashCompensation', null),
+        new ExifFormatter('Flash Control Mode', 'flashControlMode', null),
+        new ExifFormatter('Flash Exposure Compensation', 'flashExposureCompensation', null),
+        new ExifFormatter('Flash Focal Length', 'flashFocalLength', null),
+        new ExifFormatter('Flash Mode', 'flashMode', null),
+        new ExifFormatter('Flash Setting', 'flashSetting', null),
+        new ExifFormatter('Flash Type', 'flashType', null),
+        new ExifFormatter('Focus Distance', 'focusDistance', this.distance),
+        new ExifFormatter('Focus Mode', 'focusMode', null),
+        new ExifFormatter('Focus Position', 'focusPosition', null),
+        new ExifFormatter('High ISO Noise Reduction', 'highIsoNoiseReduction', null),
+        new ExifFormatter('Hue Adjustment', 'hueAdjustment', null),
+        new ExifFormatter('Noise Reduction', 'noiseReduction', null),
+        new ExifFormatter('Picture Control', 'pictureControlName', null),
+        new ExifFormatter('Primary AF Point', 'primaryAFPoint', null),
+        new ExifFormatter('VR Mode', 'vrMode', null),
+        new ExifFormatter('Vibration Reduction', 'vibrationReduction', null),
+        new ExifFormatter('Vignette Control', 'vignetteControl', null),
         new ExifFormatter('White Balance', 'whiteBalance', null),
-        new ExifFormatter('Shot Taken Date', 'shotTakenDate', null),
-        new ExifFormatter('Exposure Program', 'exposureProgram', null),
-        new ExifFormatter('GPS Version ID', 'gpsVersionId', null),
-        new ExifFormatter('GPS Latitude', 'gpsLatitude', this.formatLatitude),
-        new ExifFormatter('GPS Longitude', 'gpsLongitude', this.formatLongitude),
-        new ExifFormatter('GPS Altitude', 'gpsAltitude', this.formatAltitude),
-        new ExifFormatter('GPS Time Stamp', 'gpsTime', null),
-        new ExifFormatter('GPS Satellites', 'gpsSatellites', null)];
+        new ExifFormatter('Aperture', 'aperture', null),
+        new ExifFormatter('Auto Focus', 'autoFocus', null),
+        new ExifFormatter('Depth Of Field', 'depthOfField', null),
+        new ExifFormatter('Field of View', 'fieldOfView', null),
+        new ExifFormatter('Hyperfocal Distance', 'hyperfocalDistance', this.distance),
+        new ExifFormatter('Lens ID', 'lensId', null),
+        new ExifFormatter('Light Value', 'lightValue', this.fourDecimals),
+        new ExifFormatter('Scale Factor 35 EFL', 'scaleFactor35Efl', null),
+        new ExifFormatter('Shutter Speed', 'shutterSpeed', null),
+        new ExifFormatter('Raw Conversion Mode', 'rawConversionMode', null),
+        new ExifFormatter('Sigmoidal Contrast Adjustment', 'sigmoidalContrastAdjustment', this.fourDecimals),
+        new ExifFormatter('Saturation Adjustment', 'saturationAdjustment', this.fourDecimals),
+        new ExifFormatter('Compression Quality', 'compressionQuality', null),
+    ];
 
     exif: Array<Array<ExifDetail>> = [];
 
@@ -96,24 +122,8 @@ export class ExifViewComponent {
         return val.toString();
     }
 
-    inverseTime(val: number): string {
-        if (val >= 1) {
-            return val.toString();
-        }
-
-        return `1/${Math.round(1.0 / val)}`;
-    }
-
-    focalLengthOneDecimal(val: number): string {
-        return `${val.toFixed(1)} mm`;
-    }
-
-    focalLengthNoDecimals(val: number): string {
-        return `${val} mm`;
-    }
-
-    oneDecimal(val: number): string {
-        return val.toFixed(1);
+    fourDecimals(val: number): string {
+        return val.toFixed(4);
     }
 
     distance(val: number): string {

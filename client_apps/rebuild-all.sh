d920565a2d0217f25d78c4267df3b9420c2926f3
Mike Morano <mmorano@mikeandwan.us>
2016-07-28 14:44:01 -0400

    include the webgl demos in the build

--- client_apps/rebuild-all.sh
+++ client_apps/rebuild-all.sh
@@ -1,8 +1,15 @@
 apps=( "bandwidth" "binary_clock" "byte_counter" "filesize" "googlemaps" "learning" "memory" "money_spin" "ng_maw" "photos" "time" "videos" "weekend_countdown" )
+webgls=( "webgl_cube" "webgl_text" )
 
 build() {
     cd "${1}"
     ng build && ng lint
+    cd ..
+}
+
+buildgl() {
+    cd "${1}"
+    tsc
     cd ..
 }
 
@@ -14,5 +21,11 @@ if [ "${dowork}" == "y" ]; then
     do
         echo "building ${i}..."
         build "${i}"
+    done
+
+    for i in "${webgls[@]}"
+    do
+        echo "building ${i}..."
+        buildgl "${i}"
     done
 fi

83c134120de208b25a56c1f4bedd7a02ad132428
Mike Morano <mmorano@mikeandwan.us>
2016-08-08 05:51:28 -0400

    copy rather than ln, which fixes this for webpack

--- client_apps/videos/src/link_ngmaw.sh
+++ client_apps/videos/src/link_ngmaw.sh
@@ -1,1 +1,5 @@
-ln -s ../../ng_maw/src/app/ ng_maw
+# worked before webpack, try to bring this back later
+#ln -s ../../ng_maw/src/app/ ng_maw
+
+rm -rf ./ng_maw
+cp -R ../../ng_maw/src/app/ ./ng_maw

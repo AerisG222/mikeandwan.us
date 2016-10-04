ec4c04926752e25a19f41a00d1eb09a616b8d72f
Mike Morano <mmorano@mikeandwan.us>
2016-10-04 06:06:44 -0400

    cleanup todos

--- mikeandwan.us/Startup.cs
+++ mikeandwan.us/Startup.cs
@@ -33,8 +33,6 @@ using MawMvcApp.ViewModels.About;
 
 namespace MawMvcApp
 {
-    // TODO: review/fix bootstrap accordian panels (right sidebar), animation is too fast on FF - ok on chrome
-    // TODO: research viewcomponents
     // TODO: googlemaps add async defer back and handle callback when it loads
     public class Startup
     {

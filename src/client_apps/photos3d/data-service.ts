9142fdb065ce4db0c2f74d038ce6f4b0181cd7e8
Mike Morano <mmorano@mikeandwan.us>
2016-10-22 07:50:59 -0400

    add in the background

--- src/client_apps/photos3d/data-service.ts
+++ src/client_apps/photos3d/data-service.ts
@@ -1,25 +1,40 @@
 import { ICategory } from './icategory';
 import { IPhoto } from './iphoto';
-import { Promise } from 'es6-promise';
 
 export class DataService {
+    reqOpts = { method: 'get', credentials: 'include' };
+
     getCategories(): Promise<Array<ICategory>> {
-        return window.fetch('/api/photos/getAllCategories3D', {
-                method: 'get'
-            }).then((response: Array<ICategory>) => {
-                return response; 
-            }).catch((err: Error) => {
+        return window.fetch('/api/photos/getAllCategories3D', this.reqOpts)
+            .then(this.status)
+            .then(this.json)
+            .then(data => data)
+            .catch((err: Error) => {
                 alert(`There was an error getting data: {err}`);
             });
     }
 
     getPhotos(categoryId: number): Promise<Array<IPhoto>> {
-        return window.fetch(`/api/photos/getPhotos3D/{categoryId}`, {
-                method: 'get'
-            }).then((response: Array<IPhoto>) => {
-                return response;
-            }).catch((err: Error) => {
-                alert("There was an error getting data: {err}");
+        return window.fetch(`/api/photos/getPhotos3D/{categoryId}`, this.reqOpts)
+            .then(this.status)
+            .then(this.json)
+            .then(data => data)
+            .catch((err: Error) => {
+                alert(`There was an error getting data: {err}`);
             });
+    }
+
+    // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+    private status(response) {
+        if(response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response);
+        }
+        else {
+            return Promise.reject(new Error(response.statusText))
+        }
+    }
+
+    private json(response) {
+        return response.json();
     }
 }

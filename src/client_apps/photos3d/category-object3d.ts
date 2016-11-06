c95a06b1482158ff4cfc2d368e2844acc29d056a
Mike Morano <mmorano@mikeandwan.us>
2016-11-06 09:25:54 -0500

    try to load the images

--- src/client_apps/photos3d/category-object3d.ts
+++ src/client_apps/photos3d/category-object3d.ts
@@ -3,19 +3,24 @@ import { ICategory } from './icategory';
 export class CategoryObject3D extends THREE.Object3D {
     mesh: THREE.Mesh = null;
 
-    constructor(category: ICategory,
-                endPosition: THREE.Vector3,
+    constructor(private category: ICategory,
+                private endPosition: THREE.Vector3,
                 public color: number) {
         super();
     }
 
     init() {
+        let loader = new THREE.TextureLoader();
         let geometry = new THREE.BoxGeometry(32, 32, 1);
         let material = new THREE.MeshLambertMaterial({
             color: this.color
         });
 
         this.mesh = new THREE.Mesh(geometry, material);
+
+        loader.load(this.category.teaserImage.path, texture => {
+            this.mesh.material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+        });
 
         this.add(this.mesh);
     }

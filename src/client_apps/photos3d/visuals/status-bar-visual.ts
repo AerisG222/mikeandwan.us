f0fc1475b5b3a6f7d8ea4e4612ab2a87518508e3
Mike Morano <mmorano@mikeandwan.us>
2016-12-21 09:06:35 -0500

    add a button to get to the help screen

--- src/client_apps/photos3d/visuals/status-bar-visual.ts
+++ src/client_apps/photos3d/visuals/status-bar-visual.ts
@@ -4,10 +4,11 @@ import { ActiveStatus } from '../models/active-status';
 import { StatusBarController } from '../controllers/status-bar-controller';
 
 export class StatusBarVisual implements IVisual {
-    rootDiv: HTMLDivElement;
-    actualDiv: HTMLDivElement;
-    temporalDiv: HTMLDivElement;
-    statusDiv: HTMLDivElement;
+    private _rootDiv: HTMLDivElement;
+    private _actualDiv: HTMLDivElement;
+    private _temporalDiv: HTMLDivElement;
+    private _statusDiv: HTMLDivElement;
+    private _helpButton: HTMLButtonElement;
 
     constructor(private _controller: StatusBarController) {
         if (_controller == null) {
@@ -16,30 +17,35 @@ export class StatusBarVisual implements IVisual {
     }
 
     init() {
-        this.rootDiv = document.createElement('div');
-        this.rootDiv.id = 'nav';
-        this.rootDiv.style.position = 'absolute';
-        this.rootDiv.style.bottom = '0';
-        this.rootDiv.style.opacity = '0.7';
-        this.rootDiv.style.backgroundColor = '#333';
-        this.rootDiv.style.height = '24px';
-        this.rootDiv.style.width = '100%';
-        this.rootDiv.style.fontWeight = 'bold';
+        this._rootDiv = document.createElement('div');
+        this._rootDiv.id = 'nav';
+        this._rootDiv.style.position = 'absolute';
+        this._rootDiv.style.bottom = '0';
+        this._rootDiv.style.opacity = '0.7';
+        this._rootDiv.style.backgroundColor = '#333';
+        this._rootDiv.style.height = '24px';
+        this._rootDiv.style.width = '100%';
+        this._rootDiv.style.fontWeight = 'bold';
 
-        document.body.appendChild(this.rootDiv);
+        document.body.appendChild(this._rootDiv);
 
-        this.actualDiv = this.createInfoDiv('33%', 'left');
-        this.actualDiv.style.padding = '2px 8px';
-        this.rootDiv.appendChild(this.actualDiv);
+        this._actualDiv = this.createInfoDiv('33.333333%', '2px 8px');
+        this._rootDiv.appendChild(this._actualDiv);
 
-        this.temporalDiv = this.createInfoDiv('33%', 'left');
-        this.temporalDiv.style.padding = '2px 8px';
-        this.temporalDiv.style.textAlign = 'center';
-        this.rootDiv.appendChild(this.temporalDiv);
+        this._temporalDiv = this.createInfoDiv('33.333333%', '2px 8px', 'center');
+        this._rootDiv.appendChild(this._temporalDiv);
 
-        this.statusDiv = this.createInfoDiv('33%', 'right');
-        this.statusDiv.style.padding = '2px 8px';
-        this.rootDiv.appendChild(this.statusDiv);
+        this._statusDiv = this.createInfoDiv('33.333333%', '0 8px', 'right');
+        this._rootDiv.appendChild(this._statusDiv);
+
+        this._helpButton = document.createElement('button');
+        this._helpButton.type = 'button';
+        this._helpButton.classList.add('btn', 'btn-primary', 'btn-xs');
+        this._helpButton.setAttribute('data-toggle', 'modal');
+        this._helpButton.setAttribute('data-target', '#help');
+        this._helpButton.innerText = '?';
+        this._helpButton.style.fontWeight = 'bold';
+        this._statusDiv.appendChild(this._helpButton);
     }
 
     render(clockDelta: number, elapsed: number) {
@@ -51,22 +57,22 @@ export class StatusBarVisual implements IVisual {
     }
 
     show() {
-        this.rootDiv.style.display = 'block';
+        this._rootDiv.style.display = 'block';
     }
 
     hide() {
-        this.rootDiv.style.display = 'none';
+        this._rootDiv.style.display = 'none';
     }
 
     updateTemporal(category: string) {
-        this.temporalDiv.innerHTML = category;
+        this._temporalDiv.innerHTML = category;
     }
 
     updateActive(status: ActiveStatus) {
         if (status.category != null) {
-            this.actualDiv.innerHTML = `${status.year} | ${status.category}`;
+            this._actualDiv.innerHTML = `${status.year} | ${status.category}`;
         } else {
-            this.actualDiv.innerHTML = status.year.toString();
+            this._actualDiv.innerHTML = status.year.toString();
         }
     }
 
@@ -74,11 +80,19 @@ export class StatusBarVisual implements IVisual {
         // TODO: anything we want to show here?  fps? help/keybindings? exit? about?
     }
 
-    private createInfoDiv(width: string, align: string) {
+    private createInfoDiv(width: string, padding?: string, textAlign?: string) {
         let div = document.createElement('div');
 
         div.style.width = width;
         div.style.cssFloat = 'left';
+
+        if(padding != null) {
+            div.style.padding = padding;
+        }
+
+        if(textAlign != null) {
+            div.style.textAlign = textAlign;
+        }
 
         return div;
     }

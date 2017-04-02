165b2fd41a6df27078ad5173a1f503bf521bc5c9
Mike Morano <mmorano@mikeandwan.us>
2017-04-02 15:02:05 -0400

    enable ssl in development using test cert

--- src/mikeandwan.us/Program.cs
+++ src/mikeandwan.us/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,14 +10,29 @@ namespace MawMvcApp
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
+            var isDevelopment = string.Equals("development", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), StringComparison.OrdinalIgnoreCase);
+            var host = new WebHostBuilder();
+            
+            if(isDevelopment)
+            {
+                host
+                    .UseKestrel(opts => 
+                    {
+                        opts.UseHttps("test_certs/testcert.pfx", "TestCertificate");
+                    })
+                    .UseUrls("http://localhost:5000", "https://localhost:5001");
+            }
+            else
+            {
+                host.UseKestrel();
+            }
+            
+            host
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .CaptureStartupErrors(true)
                 .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+                .Build()
+                .Run();
         }
     }
 }

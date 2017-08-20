1a67c34db8390d31ec4ab4e3088d558aa237ec90
Mike Morano <mmorano@mikeandwan.us>
2017-08-20 09:20:01 -0400

    workaround for error with prod build

--- src/client_apps/photos/src/app/shared/route-mode.model.ts
+++ src/client_apps/photos/src/app/shared/route-mode.model.ts
@@ -1,4 +1,5 @@
 export enum RouteMode {
+    None,  // this was added and magically provides a workaround for https://github.com/angular/angular/issues/18170
     Category,
     Comment,
     Rating,

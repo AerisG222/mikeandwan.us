c7ee88b66cc116e4f969dbc006fa1d68c3995e5c
Mike Morano <mmorano@mikeandwan.us>
2018-03-23 17:23:31 -0400

    add tag helpers to simplify linking between www and auth, while accounting for environment

---
+++ src/Maw.TagHelpers/UrlAuthTagHelper.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace Maw.TagHelpers
+{
+	[HtmlTargetElement("a", Attributes = AttributeName)]
+	public class UrlAuthTagHelper
+		: TagHelper
+	{
+		const string AttributeName = "maw-auth-url";
+        static readonly Uri LocalBaseUri = new Uri("https://localhost:5001/");
+        static readonly Uri ProductionBaseUri = new Uri("https://auth.mikeandwan.us/");
+
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+
+		[HtmlAttributeName(AttributeName)]
+		public string Url { get; set; }
+
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+            Uri dest = null;
+			var req = ViewContext.HttpContext.Request.Host;
+
+            if(string.Equals(req.Host, "localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                dest = new Uri(LocalBaseUri, Url);
+            }
+            else
+            {
+                dest = new Uri(ProductionBaseUri, Url);
+            }
+
+			output.Attributes.SetAttribute("href", dest);
+		}
+	}
+}

b2538e6a170a1ea597a1907c421f038da918dc3b
Mike Morano <mmorano@mikeandwan.us>
2018-05-20 17:18:54 -0400

    update paths

--- tools/link_media.sh
+++ tools/link_media.sh
@@ -1,9 +1,9 @@
 MEDIADIR=/srv/www/website_assets
 SRCDIR=/home/mmorano/git/mikeandwan.us/src
 
-if [ ! -L "${SRCDIR}/mikeandwan.us/wwwroot/images" ]; then
-    ln -s "${MEDIADIR}/images" "${SRCDIR}/mikeandwan.us/wwwroot/images"
+if [ ! -L "${SRCDIR}/www/wwwroot/images" ]; then
+    ln -s "${MEDIADIR}/images" "${SRCDIR}/www/wwwroot/images"
 fi
-if [ ! -L "${SRCDIR}/mikeandwan.us/wwwroot/movies" ]; then
-    ln -s "${MEDIADIR}/movies" "${SRCDIR}/mikeandwan.us/wwwroot/movies"
+if [ ! -L "${SRCDIR}/www/wwwroot/movies" ]; then
+    ln -s "${MEDIADIR}/movies" "${SRCDIR}/www/wwwroot/movies"
 fi

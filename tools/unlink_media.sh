8208a378c2e0096af22a70634640efad0aa439c7
Mike Morano <mmorano@mikeandwan.us>
2018-07-18 06:47:44 -0400

    add script to undo links

---
+++ tools/unlink_media.sh
@@ -0,0 +1,8 @@
+SRCDIR=/home/mmorano/git/mikeandwan.us/src
+
+if [ -L "${SRCDIR}/www/wwwroot/images" ]; then
+    rm "${SRCDIR}/www/wwwroot/images"
+fi
+if [ -L "${SRCDIR}/www/wwwroot/movies" ]; then
+    rm "${SRCDIR}/www/wwwroot/movies"
+fi

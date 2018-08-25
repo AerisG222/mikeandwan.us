1328eefdd9a6ac952a5ad7d1f861354a6b4619d7
Mike Morano <mmorano@mikeandwan.us>
2018-08-25 16:48:23 -0400

    update the uploader display, heavily leveraging the demo of ng2-file-upload

--- src/client_apps/upload/src/app/upload/upload.component.ts
+++ src/client_apps/upload/src/app/upload/upload.component.ts
@@ -4,13 +4,19 @@ import { Observable } from 'rxjs';
 import { UploadState } from '../state/upload.state';
 import { Select, Store } from '@ngxs/store';
 import { InitializeUploader } from '../state/upload.actions';
+import { FileSizePipe } from '../pipes/file-size.pipe';
 
 @Component({
     selector: 'app-upload',
     templateUrl: './upload.component.html',
-    styleUrls: ['./upload.component.css']
+    styleUrls: ['./upload.component.css'],
+    providers: [
+        FileSizePipe
+    ]
 })
 export class UploadComponent implements OnInit {
+    hasBaseDropZoneOver = false;
+
     @Select(UploadState.getUploader) uploader$: Observable<FileUploader>;
 
     constructor(private _store: Store) {
@@ -19,5 +25,9 @@ export class UploadComponent implements OnInit {
 
     ngOnInit() {
         this._store.dispatch(new InitializeUploader());
+    }
+
+    public fileOverBase(e: any): void {
+        this.hasBaseDropZoneOver = e;
     }
 }

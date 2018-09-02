8542648409317d3c25623a48050bc03e870cf994
Mike Morano <mmorano@mikeandwan.us>
2018-09-02 19:55:45 -0400

    does not work, starting to try and use signalr to pull the initial file list

--- src/client_apps/upload/src/app/services/upload.service.ts
+++ src/client_apps/upload/src/app/services/upload.service.ts
@@ -1,26 +1,79 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { User } from 'oidc-client';
+import { Observable,  BehaviorSubject, from, Subject } from 'rxjs';
+import { filter, tap, map, switchMap, first, catchError } from 'rxjs/operators';
+import * as signalR from '@aspnet/signalr';
+
 import { IFileInfo } from '../models/ifile-info';
 import { EnvironmentConfig } from '../models/environment-config';
+import { AuthState } from '../state/auth.state';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UploadService {
-    constructor(private _http: HttpClient,
-                private _cfg: EnvironmentConfig) {
+    private _initSubject$ = new Subject();
+    private _hub: signalR.HubConnection;
+    private _x: any;
 
+    @Select(AuthState.getUser) private _user$: Observable<User>;
+
+    constructor(private _cfg: EnvironmentConfig,
+                private _store: Store) {
+        this._x = this._user$
+            .pipe(
+                tap(x => console.log('user: ', x)),
+                filter(user => user !== undefined)
+            )
+            .subscribe(
+                user => this.setupSignalrHub(user),
+                err => console.log(err)
+            );
     }
 
-    getServerFiles(): Observable<Array<IFileInfo>> {
+    getServerFiles(): Observable<IFileInfo[]> {
+        /* webapi call:
         const url = this.getAbsoluteUrl('upload/files');
 
         return this._http
             .get<Array<IFileInfo>>(url);
+        */
+
+        return this._user$
+            .pipe(
+                switchMap((user, idx) =>
+                    from(this._hub
+                        .invoke('GetAllFiles')
+                    )
+                )
+            );
     }
 
     getAbsoluteUrl(relativeUrl: string) {
         return `${this._cfg.apiUrl}/${relativeUrl}`;
+    }
+
+    private setupSignalrHub(user: User) {
+        // tslint:disable-next-line:no-console
+        console.log('setting up signalr hub...');
+
+        const tokenValue = `?token=${user.access_token}`;
+
+        this._hub = new signalR.HubConnectionBuilder()
+            .withUrl(`${this.getAbsoluteUrl('uploadr')}${tokenValue}`)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        this._hub.start().catch(err => console.error(err.toString()));
+
+        this._hub.on('FileAdded', (uploadedFile: IFileInfo) => {
+
+            // this._store.dispatch(new NewsActions.ReceivedItemAction(newsItem));
+        });
+
+        this._hub.on('FileDeleted', (uploadedFile: IFileInfo) => {
+            // this._store.dispatch(new NewsActions.ReceivedGroupJoinedAction(data));
+        });
     }
 }

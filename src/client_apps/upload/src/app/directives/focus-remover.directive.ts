f5dcef666fbfd4d09f7620f96b84176a7e6ed775
Mike Morano <mmorano@mikeandwan.us>
2018-09-23 10:03:05 -0400

    remove use of deprecated renderer (and do not move to renderer2 as this function was deliberately dropped)

--- src/client_apps/upload/src/app/directives/focus-remover.directive.ts
+++ src/client_apps/upload/src/app/directives/focus-remover.directive.ts
@@ -1,16 +1,15 @@
-import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 // https://gist.github.com/AlejandroPerezMartin/ecd014cb8104c235b582f3a3e1649cf7
 @Directive({
     selector: '[appFocusRemover]'
 })
 export class FocusRemoverDirective {
-    constructor(private elRef: ElementRef,
-        private renderer: Renderer) {
+    constructor(private _elRef: ElementRef) {
 
     }
 
     @HostListener('click') onClick() {
-        this.renderer.invokeElementMethod(this.elRef.nativeElement, 'blur', []);
+        this._elRef.nativeElement.blur();
     }
 }

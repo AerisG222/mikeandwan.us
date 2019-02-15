f204fec2eafa95d3768a426215883f9caab3c6f8
Mike Morano <mmorano@mikeandwan.us>
2019-02-15 10:17:16 -0500

    delete unused class

--- src/Maw.Domain/Photos/GpsData.cs
+++
@@ -1,9 +0,0 @@
-namespace Maw.Domain.Photos
-{
-    public class GpsData
-    {
-		public long PhotoId { get; set; }
-		public float LatitudeDegrees { get; set; }
-		public float LongitudeDegrees { get; set; }
-	}
-}

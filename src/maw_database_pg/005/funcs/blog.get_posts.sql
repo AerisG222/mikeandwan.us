3b8fe68f6569ceb64ae519823f46239674dd050a
Mike Morano <mmorano@mikeandwan.us>
2019-06-07 05:47:27 -0400

    improve consistency

--- src/maw_database_pg/005/funcs/blog.get_posts.sql
+++ src/maw_database_pg/005/funcs/blog.get_posts.sql
@@ -22,7 +22,7 @@ AS $$
            publish_date
       FROM blog.post
      WHERE (_blog_id IS NULL OR blog_id = _blog_id)
-       AND (_id IS NULL OR _id = id)
+       AND (_id IS NULL OR id = _id)
      ORDER BY publish_date DESC
      LIMIT _post_count;
 

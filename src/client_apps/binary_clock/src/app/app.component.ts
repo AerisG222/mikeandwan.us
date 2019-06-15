cad8da9ccff56337752dbc816fbe641cea97bd99
Mike Morano <mmorano@mikeandwan.us>
2019-06-15 13:10:56 -0400

    migrate to scss

--- src/client_apps/binary_clock/src/app/app.component.ts
+++ src/client_apps/binary_clock/src/app/app.component.ts
@@ -5,7 +5,7 @@ declare var notick: boolean;
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: [ './app.component.css' ]
+    styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnDestroy {
     intervalId: number;

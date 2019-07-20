a4bd5c2a483b0b8f4e3d28075e469e96dd30923b
Mike Morano <mmorano@mikeandwan.us>
2019-07-20 07:51:28 -0400

    fix analyzer warnings in test

---
+++ tests/Maw.Data.Tests/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("", "CA1303")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("", "CA1707")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("", "CA1822")]

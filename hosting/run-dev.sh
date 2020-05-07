283f1d284cdff441029d7ca5966e073ac06b0b98
Mike Morano <mmorano@mikeandwan.us>
2020-05-07 09:37:43 -0400

    start to build out script to run the full dev env

---
+++ hosting/run-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# postgres
+podman run -dt --pod maw-pod -v maw-postgres:/var/lib/postgresql/data:rw,z postgres:12.2
+
+# solr
+podman run -dt --pod maw-pod -v maw-solr:/var/solr/data:rw,z maw-solr-dev
+
+# photos
+podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-photos-dev
+
+# files
+# podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-files-dev
+
+# auth
+# podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-auth-dev
+
+# api
+# podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-api-dev
+
+# www
+# podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-www-dev
+
+# gateway
+podman run -dt --pod maw-pod -v maw-certs:/certs:ro,z maw-gateway-dev

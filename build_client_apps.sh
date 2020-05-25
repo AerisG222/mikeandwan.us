60a0f94a90590d06e69cf7e6cbc38405a16bb4cf
Mike Morano <mmorano@mikeandwan.us>
2020-05-25 13:54:02 -0400

    update client app scripts

---
+++ build_client_apps.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+PROD=$1
+
+APPS=(
+    "webgl_blender_model"
+    "webgl_cube"
+    "webgl_shader"
+    "webgl_text"
+)
+
+
+clean_app() {
+    rm -rf dist
+    rm -rf node_modules
+    npm ci
+}
+
+build_app() {
+    local app=$1
+    local buildcmd=$2
+    local clean=$3
+
+    pushd "${app}"
+
+    if [ "${clean}" == 'y' ]; then
+        echo "cleaning ${app}..."
+        clean_app
+    fi
+
+    echo "building ${app}..."
+    npm run "${buildcmd}"
+
+    popd
+}
+
+build_ng_apps() {
+    local buildcmd=$1
+    local clean=$2
+
+    if [ "${clean}" == 'y' ]; then
+        clean_app
+    fi
+
+    echo "building ${app}..."
+    npm run "${buildcmd}"
+}
+
+build_all_apps() {
+    local prod=$1
+
+    pushd src/client_apps
+
+    for i in "${APPS[@]}"
+    do
+        if [ "${prod}" == 'y' ]; then
+            build_app "${i}" prod_build y
+        else
+            build_app "${i}" dev_build n
+        fi
+    done
+
+    popd
+
+    pushd src/client_apps_ng
+
+    if [ "${prod}" == 'y' ]; then
+        build_ng_apps build-prod y
+    else
+        build_ng_apps build-dev n
+    fi
+
+    popd
+}
+
+build_all_apps "${PROD}"
--- src/client_apps/update-cli.sh
+++
@@ -1,9 +0,0 @@
-source _vars.sh
-
-# 1: update global tooling
-echo 'Update global ng cli? [y/n]'
-read GLOBAL_UPDATE
-
-if [ "${GLOBAL_UPDATE}" == "y" ]; then
-    update_ngcli_global
-fi

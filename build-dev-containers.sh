64ff101c3efdf157a7e5c9e56052da2dfc8b7aae
Mike Morano <mmorano@mikeandwan.us>
2020-05-30 09:24:47 -0400

    move and rename script to build dev containers

--- build-dev-containers.sh
+++ build-dev-containers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd hosting
+
 pushd certs
 buildah bud -f Containerfile.dev -t maw-certs-dev
 popd
@@ -9,7 +11,7 @@ buildah bud -f Containerfile.dev -t maw-gateway-dev
 popd
 
 pushd postgres-maintenance
-buildah bud -f Containerfile -t maw-postgres-maintenance
+buildah bud -f Containerfile -t maw-postgres-maintenance-dev
 popd
 
 pushd solr
@@ -17,12 +19,12 @@ buildah bud -f Containerfile -t maw-solr-dev
 popd
 
 pushd solr-reindex
-buildah bud -f Containerfile -t maw-solr-reindex
+buildah bud -f Containerfile -t maw-solr-reindex-dev
 popd
 
-pushd ..
+popd
+
 # volume mount is a workaround for a current COPY perf issue
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api-dev
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth-dev
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.www -t maw-www-dev
-popd

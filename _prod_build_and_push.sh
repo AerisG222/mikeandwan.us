17012e0a9d39438cf0c51a2322317babdcce6673
Mike Morano <mmorano@mikeandwan.us>
2020-05-30 14:25:36 -0400

    push both unique and latest tags to dockerhub

--- _prod_build_and_push.sh
+++ _prod_build_and_push.sh
@@ -8,30 +8,35 @@ pushd certs
 buildah bud -f Containerfile.prod -t maw-certs --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:latest
 popd
 
 pushd gateway
 buildah bud -f Containerfile.prod -t maw-gateway --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-gateway:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-gateway:latest
 popd
 
 pushd postgres-maintenance
 buildah bud -f Containerfile -t maw-postgres-maintenance --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-postgres-maintenance:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-postgres-maintenance:latest
 popd
 
 pushd solr
 buildah bud -f Containerfile -t maw-solr --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr:latest
 popd
 
 pushd solr-reindex
 buildah bud -f Containerfile -t maw-solr-reindex --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:latest
 popd
 
 popd
@@ -40,11 +45,14 @@ popd
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:latest
 
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:latest
 
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.www -t maw-www --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:latest

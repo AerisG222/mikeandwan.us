ead67f5ca06cfc9ee80e93a7375833ec9bc89678
Mike Morano <mmorano@mikeandwan.us>
2020-06-06 08:32:17 -0400

    remove references to containers which were migrated to their own repo

--- _prod_build_and_push.sh
+++ _prod_build_and_push.sh
@@ -2,45 +2,6 @@
 IID_FILE=image.iid
 TAG=$(date +%Y%m%d%H%M%S)
 
-pushd hosting
-
-pushd certs
-buildah bud -f Containerfile.prod -t maw-certs --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:"${TAG}"
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:latest
-popd
-
-pushd gateway
-buildah bud -f Containerfile.prod -t maw-gateway --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-gateway:"${TAG}"
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-gateway:latest
-popd
-
-pushd postgres-maintenance
-buildah bud -f Containerfile -t maw-postgres-maintenance --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-postgres-maintenance:"${TAG}"
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-postgres-maintenance:latest
-popd
-
-pushd solr
-buildah bud -f Containerfile -t maw-solr --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr:"${TAG}"
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr:latest
-popd
-
-pushd solr-reindex
-buildah bud -f Containerfile -t maw-solr-reindex --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:"${TAG}"
-buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:latest
-popd
-
-popd
-
 # volume mount is a workaround for a current COPY perf issue
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
--- build-dev-containers.sh
+++ build-dev-containers.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-pushd hosting
-
-pushd certs
-buildah bud -f Containerfile.dev -t maw-certs-dev
-popd
-
-pushd gateway
-buildah bud -f Containerfile.dev -t maw-gateway-dev
-popd
-
-pushd postgres-maintenance
-buildah bud -f Containerfile -t maw-postgres-maintenance-dev
-popd
-
-pushd solr
-buildah bud -f Containerfile -t maw-solr-dev
-popd
-
-pushd solr-reindex
-buildah bud -f Containerfile -t maw-solr-reindex-dev
-popd
-
-popd
-
 # volume mount is a workaround for a current COPY perf issue
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api-dev
 buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth-dev

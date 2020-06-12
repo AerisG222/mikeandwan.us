c90d942ef2ab85c5a059ad5341a97e8f3f99d09d
Mike Morano <mmorano@mikeandwan.us>
2020-06-11 20:10:53 -0400

    update runtime to run on alpine and ditch imagemagick for much smaller image size

--- _start_site_in_container.sh
+++ _start_site_in_container.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-# trust the self signed CA certs
-cp /certs/internal/ca/ca_*.crt /usr/share/pki/ca-trust-source/anchors/
-update-ca-trust
+# trust the self signed CA certs (fedora)
+#cp /certs/internal/ca/ca_*.crt /usr/share/pki/ca-trust-source/anchors/
+#update-ca-trust
+
+# trust the self signed CA certs (Alpine)
+# credit: https://hackernoon.com/alpine-docker-image-with-secured-communication-ssl-tls-go-restful-api-128eb6b54f1f
+cp /certs/internal/ca/ca_*.crt /usr/local/share/ca-certificates
+update-ca-certificates
 
 exec "$@"

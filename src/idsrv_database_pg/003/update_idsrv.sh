c251544a5c212adc1f7bd1fb03dd076d665827fd
Mike Morano <mmorano@mikeandwan.us>
2020-06-19 17:44:10 -0400

    update script to apply to container

--- src/idsrv_database_pg/003/update_idsrv.sh
+++ src/idsrv_database_pg/003/update_idsrv.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 DBNAME="idsrv";
+PODNAME=$1
+ENVFILE=$2
 
 function run_psql_script() {
-    psql -d "${DBNAME}" -q -f "$1";
+    if [ "${PODNAME}" == "" ]; then
+        psql -d "${DBNAME}" -q -f "$1";
+    else
+        podman run -it --rm --pod "${PODNAME}" --env-file "${ENVFILE}" -v "$(pwd)":/tmp/context:ro --security-opt label=disable postgres:12.2 psql -h 127.0.0.1 -U postgres -d idsrv -f "/tmp/context/${1}"
+    fi
 }
 
 echo 'updating tables...';
@@ -16,3 +22,6 @@ run_psql_script "funcs/idsrv.get_persisted_grants.sql";
 run_psql_script "funcs/idsrv.save_persisted_grant.sql";
 
 echo "...${DBNAME} updated.";
+
+# for podman - run as:
+# ./update_idsrv.sh <pod_name> <path_to_pgbackup_env_file>

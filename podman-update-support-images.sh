91b8083ffbbe00b483618fd2c468f0368950673b
Mike Morano <mmorano@mikeandwan.us>
2021-05-25 06:27:02 -0400

    add scrpit to help pull new images for support services

---
+++ podman-update-support-images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+podman pull docker.io/aerisg222/maw-certs
+podman pull docker.io/aerisg222/maw-postgres-maintenance
+podman pull docker.io/aerisg222/maw-reverse-geocode
+podman pull docker.io/aerisg222/maw-solr-reindex

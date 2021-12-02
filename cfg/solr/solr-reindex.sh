f9aebb7759b325d3e5600c8083cdeb35cf50baae
Mike Morano <mmorano@mikeandwan.us>
2021-12-02 09:32:22 -0500

    solr config moving to maw-solr repo

--- cfg/solr/solr-reindex.sh
+++
@@ -1,5 +0,0 @@
-# delete all documents
-curl -X POST -H 'Content-Type: application/json' --data-binary '{"delete":{"query":"*:*" }}' http://localhost:8983/solr/multimedia-categories/update/?commit=true
-
-# kick off re-index
-curl http://localhost:8983/solr/multimedia-categories/dataimport?command=full-import

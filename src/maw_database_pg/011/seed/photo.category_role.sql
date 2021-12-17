0d1be64044a4c9cd7a4b43b8cf2798b792525a60
Mike Morano <mmorano@mikeandwan.us>
2021-12-17 15:10:35 -0500

    allow this to be run multiple times

--- src/maw_database_pg/011/seed/photo.category_role.sql
+++ src/maw_database_pg/011/seed/photo.category_role.sql
@@ -2,13 +2,17 @@ DO
 $$
 BEGIN
 
-    INSERT INTO photo.category_role
-    SELECT pc.id AS category_id,
-           r.id AS role_id
-      FROM photo.category pc
-     CROSS JOIN maw.role r
-     WHERE r.name = 'admin'
-        OR (r.name = 'friend' and pc.is_private = FALSE);
+    IF NOT EXISTS (SELECT 1 FROM photo.category_role) THEN
+
+        INSERT INTO photo.category_role
+        SELECT pc.id AS category_id,
+            r.id AS role_id
+        FROM photo.category pc
+        CROSS JOIN maw.role r
+        WHERE r.name = 'admin'
+            OR (r.name = 'friend' and pc.is_private = FALSE);
+
+    END IF;
 
 END
 $$
--- src/maw_database_pg/011/seed/video.category_role.sql
+++ src/maw_database_pg/011/seed/video.category_role.sql
@@ -2,13 +2,17 @@ DO
 $$
 BEGIN
 
-    INSERT INTO video.category_role
-    SELECT vc.id AS category_id,
-           r.id AS role_id
-      FROM video.category vc
-     CROSS JOIN maw.role r
-     WHERE r.name = 'admin'
-        OR (r.name = 'friend' and vc.is_private = FALSE);
+    IF NOT EXISTS (SELECT 1 FROM video.category_role) THEN
+
+        INSERT INTO video.category_role
+        SELECT vc.id AS category_id,
+            r.id AS role_id
+        FROM video.category vc
+        CROSS JOIN maw.role r
+        WHERE r.name = 'admin'
+            OR (r.name = 'friend' and vc.is_private = FALSE);
+
+    END IF;
 
 END
 $$

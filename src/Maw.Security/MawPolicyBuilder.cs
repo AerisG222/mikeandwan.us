ea72df69910ed1e77116358d77b419de36c2fbbd
Mike Morano <mmorano@mikeandwan.us>
2021-12-21 07:27:44 -0500

    let all autheticated users have read access to photos/videos and let those services limit content based on role membership

--- src/Maw.Security/MawPolicyBuilder.cs
+++ src/Maw.Security/MawPolicyBuilder.cs
@@ -13,8 +13,8 @@ namespace Maw.Security
                 throw new ArgumentNullException(nameof(opts));
             }
 
-            opts.AddPolicy(MawPolicy.ViewPhotos, new AuthorizationPolicyBuilder().RequireRole(Role.Friend, Role.Admin).Build());
-            opts.AddPolicy(MawPolicy.ViewVideos, new AuthorizationPolicyBuilder().RequireRole(Role.Friend, Role.Admin).Build());
+            opts.AddPolicy(MawPolicy.ViewPhotos, new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build());
+            opts.AddPolicy(MawPolicy.ViewVideos, new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build());
             opts.AddPolicy(MawPolicy.AdminPhotos, new AuthorizationPolicyBuilder().RequireRole(Role.Admin).Build());
             opts.AddPolicy(MawPolicy.AdminVideos, new AuthorizationPolicyBuilder().RequireRole(Role.Admin).Build());
             opts.AddPolicy(MawPolicy.AdminSite, new AuthorizationPolicyBuilder().RequireRole(Role.Admin).Build());

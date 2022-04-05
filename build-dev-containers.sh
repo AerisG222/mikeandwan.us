78cde01fc7176d7f167f2d3bc9fe020736eb4b1a
Mike Morano <mmorano@mikeandwan.us>
2022-04-05 19:56:19 -0400

    rename to make more consistent with our updated environment naming standards

--- build-dev-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# volume mount is a workaround for a current COPY perf issue
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api-dev
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth-dev
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.www -t maw-www-dev
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.cache -t maw-cache-sync-dev

82a108217b411c99ba0578e421bcba1d76019c48
Mike Morano <mmorano@mikeandwan.us>
2022-06-12 12:07:39 -0400

    update the video script to use containers

--- tools/publish_videos.sh
+++ tools/publish_videos.sh
@@ -7,17 +7,29 @@
 DEBUG=y
 SSH_REMOTE_HOST=tifa
 SSH_USERNAME=svc_www_maw
-PATH_CONVERT_VIDEOS="/home/mmorano/git/ConvertVideos/src/ConvertVideos/bin/Release/net6.0/ConvertVideos.dll"
-PATH_GLACIER_BACKUP="/home/mmorano/git/GlacierBackup/src/GlacierBackup/bin/Release/net6.0/GlacierBackup.dll"
 PATH_ASSET_ROOT="/srv/www/website_assets"
 PATH_VIDEO_SOURCE=
 CAT_NAME=
 YEAR=
 ALLOWED_ROLES=
 
+IMG_CONVERT_PHOTOS=docker.io/aerisg222/maw-convert-videos
+IMG_GLACIER_BACKUP=docker.io/aerisg222/maw-glacier-backup
+IMG_POSTGRES=docker.io/postgres:14-alpine
+CPUS_CONVERT_VIDEOS=28
+CPUS_GLACIER_BACKUP=12
+
+DEV_POD=dev-maw-pod
+DEV_SVC="pod-${DEV_POD}"
+DEV_PSQL_ENV_FILE=/home/mmorano/maw_dev/podman-env/maw-postgres.env
+
+PROD_POD=prod-maw-pod
+PROD_PSQL_ENV_FILE=/home/svc_www_maw/maw_prod/podman-env/maw-postgres.env
+
 get_value() {
     local prompt=$1
     local secure=$2
+    local default=$3
     local val=
 
     while [ "${val}" = "" ]
@@ -26,6 +38,10 @@ get_value() {
             read -e -r -s -p "${prompt}" val
         else
             read -e -r -p "${prompt}" val
+        fi
+
+        if [ "${val}" = "" -a "${default}" != "" ]; then
+            val="${default}"
         fi
     done
 
@@ -50,7 +66,7 @@ if [ "${YEAR}" = "" ]; then
     YEAR=$(get_value 'Category year: ' 'n')
 fi
 
-roles=($(get_value 'Allowed roles (space delimited list): '))
+roles=($(get_value 'Allowed roles (space delimited list) [friend admin]: ' 'n' 'friend admin'))
 
 for i in "${roles[@]}"; do
     ALLOWED_ROLES="${ALLOWED_ROLES} -r ${i} "
@@ -84,8 +100,6 @@ if [ "${DEBUG}" = "y" ]; then
     echo "                      DEBUG: ${DEBUG}"
     echo "            SSH_REMOTE_HOST: ${SSH_REMOTE_HOST}"
     echo "               SSH_USERNAME: ${SSH_USERNAME}"
-    echo "        PATH_CONVERT_VIDEOS: ${PATH_CONVERT_VIDEOS}"
-    echo "        PATH_GLACIER_BACKUP: ${PATH_GLACIER_BACKUP}"
     echo "          PATH_VIDEO_SOURCE: ${PATH_VIDEO_SOURCE}"
     echo "            PATH_ASSET_ROOT: ${PATH_ASSET_ROOT}"
     echo "                   CAT_NAME: ${CAT_NAME}"
@@ -131,7 +145,19 @@ fi
 
 echo '* processing videos...'
 
-dotnet "${PATH_CONVERT_VIDEOS}" -c "${CAT_NAME}" -o "${PATH_LOCAL_SQL_FILE}" -v "${PATH_VIDEO_SOURCE}" -w "movies" -y ${YEAR} ${ALLOWED_ROLES}
+podman run -it --rm \
+    --memory 0 \
+    --cpus "${CPUS_CONVERT_VIDEOS}" \
+    --volume "${ASSET_ROOT}:/videodir:rw" \
+    --volume "${ASSET_ROOT}:/output:rw" \
+    --security-opt label=disable \
+    "${IMG_CONVERT_PHOTOS}" \
+        -c "${CAT_NAME}" \
+        -o "/output/${SQL_FILE}" \
+        -v "/videodir/${CATEGORY_DIRECTORY_NAME}" \
+        -w "movies" \
+        -y ${YEAR} \
+        ${ALLOWED_ROLES}
 
 stty sane
 
@@ -157,17 +183,56 @@ if [ ! -d "${DEST_MOVIES_YEAR_ROOT}" ]; then
     mkdir "${DEST_MOVIES_YEAR_ROOT}"
 fi
 
+echo '* make sure dev podman pod is running...'
+systemctl --user start "${DEV_SVC}"
+sleep 3s
+
 echo '* moving videos for local site...'
 mv "${PATH_VIDEO_SOURCE}" "${DEST_MOVIES_YEAR_ROOT}"
 
 echo '* applying sql to local database...'
-psql -d maw_website -f "${PATH_LOCAL_SQL_FILE}"
+podman run -it --rm \
+    --pod "${DEV_POD}" \
+    --env-file "${DEV_PSQL_ENV_FILE}" \
+    --volume "${ASSET_ROOT}:/output:ro" \
+    --security-opt label=disable \
+    "${IMG_POSTGRES}" \
+        psql \
+            -h localhost \
+            -U postgres \
+            -d maw_website \
+            -f "/output/${SQL_FILE}"
 
 echo '* backing up videos to AWS Glacier...'
-dotnet "${PATH_GLACIER_BACKUP}" glacier_backup us-east-1 videos assets "${DEST_MOVIES_CATEGORY_ROOT}" "${DEST_MOVIES_ROOT}/" videosql "${PATH_LOCAL_GLACIER_SQL_FILE}"
+podman run -it --rm \
+    --cpus "${CPUS_GLACIER_BACKUP}" \
+    --volume "/home/mmorano/.aws:/creds:ro" \
+    --volume "${DEST_MOVIES_ROOT}:/videodir:ro" \
+    --volume "${ASSET_ROOT}:/output:rw" \
+    --security-opt label=disable \
+    "${IMG_GLACIER_BACKUP}" \
+        glacier_backup \
+        us-east-1 \
+        videos \
+        assets \
+        "/videodir/${YEAR}/${CATEGORY_DIRECTORY_NAME}/" \
+        "/videodir/" \
+        videosql \
+        "/output/${GLACIER_SQL_FILE}" \
+        /creds/credentials
 
 echo '* applying glacier sql file to local database...'
-psql -d maw_website -f "${PATH_LOCAL_GLACIER_SQL_FILE}"
+podman run -it --rm \
+    --pod "${DEV_POD}" \
+    --env-file "${DEV_PSQL_ENV_FILE}" \
+    --volume "${ASSET_ROOT}:/output:ro" \
+    --security-opt label=disable \
+    "${IMG_POSTGRES}" \
+        psql \
+            -h localhost \
+            -U postgres \
+            -d maw_website \
+            -f "/output/${GLACIER_SQL_FILE}"
 
 #################################################
 ## REMOTE PROCESSING
@@ -190,8 +255,29 @@ ssh -t "${SSH_USERNAME}"@"${SSH_REMOTE_HOST}" "
     sudo chmod -R go-w '${DEST_MOVIES_CATEGORY_ROOT}'
     sudo restorecon -R '${DEST_MOVIES_CATEGORY_ROOT}'
 
-    podman run -it --rm --pod prod-maw-pod --env-file '/home/${SSH_USERNAME}/maw_prod/podman-env/maw-postgres.env' -v .:/tmp/context:ro --security-opt label=disable postgres:14-alpine psql -h localhost -U postgres -d maw_website -f '/tmp/context/${SQL_FILE}'
-    podman run -it --rm --pod prod-maw-pod --env-file '/home/${SSH_USERNAME}/maw_prod/podman-env/maw-postgres.env' -v .:/tmp/context:ro --security-opt label=disable postgres:14-alpine psql -h localhost -U postgres -d maw_website -f '/tmp/context/${GLACIER_SQL_FILE}'
+    podman run -it --rm \
+        --pod ${PROD_POD} \
+        --env-file '${PROD_PSQL_ENV_FILE}' \
+        --volume .:/sql:ro \
+        --security-opt label=disable \
+        ${IMG_POSTGRES} \
+            psql \
+                -h localhost \
+                -U postgres \
+                -d maw_website \
+                -f '/sql/${SQL_FILE}'
+
+    podman run -it --rm \
+        --pod ${PROD_POD} \
+        --env-file '${PROD_PSQL_ENV_FILE}' \
+        --volume .:/sql:ro \
+        --security-opt label=disable \
+        ${IMG_POSTGRES} \
+            psql \
+                -h localhost \
+                -U postgres \
+                -d maw_website \
+                -f '/sql/${GLACIER_SQL_FILE}'
 
     rm '${SQL_FILE}'
     rm '${GLACIER_SQL_FILE}'

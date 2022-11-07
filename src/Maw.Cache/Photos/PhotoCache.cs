54a56da98fcaa31be1fef32be01e0cd3c89509d3
Mike Morano <mmorano@mikeandwan.us>
2022-11-07 06:58:52 -0500

    try to tell redis to sort by alpha to fix issue with updating photo cache

--- src/Maw.Cache/Photos/PhotoCache.cs
+++ src/Maw.Cache/Photos/PhotoCache.cs
@@ -352,7 +352,7 @@ public class PhotoCache
 
     async Task<CacheResult<IEnumerable<Photo>>> GetPhotosInternalAsync(ITransaction tran, string setKey, bool sortByFilename)
     {
-        var by = sortByFilename ? $"{ PhotoKeys.PHOTO_HASH_KEY_PATTERN }->xs-path" : "nosort";
+        var by = sortByFilename ? $"{ PhotoKeys.PHOTO_HASH_KEY_PATTERN }->xs-path alpha" : "nosort";
         var status = GetStatusAsync(tran);
         var photos = tran.SortAsync(
             setKey,

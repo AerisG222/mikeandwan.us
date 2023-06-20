97bedc390cc866d5c1c89763c6175e222a8a9d19
Mike Morano <mmorano@mikeandwan.us>
2023-06-20 17:10:35 -0400

    now start the dev pod and use the   preferred way to run commands w/ gnome-terminal

--- tools/start-dev-services.sh
+++ tools/start-dev-services.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+systemctl --user start pod-dev-maw-pod.service
+
 gnome-terminal \
-    --tab --working-directory=/home/mmorano/git/mikeandwan.us/tools    --title=media --command="bash -c './unlink_media.sh && sleep 30s && ./link_media.sh';bash" \
-    --tab --working-directory=/home/mmorano/git/mikeandwan.us/src/auth --title=auth  --command="dotnet run" \
-    --tab --working-directory=/home/mmorano/git/mikeandwan.us/src/api  --title=api   --command="dotnet run" \
-    --tab --working-directory=/home/mmorano/git/mikeandwan.us/src/www  --title=www   --command="dotnet run" \
+    --tab \
+    --working-directory /home/mmorano/git/mikeandwan.us/tools \
+    --title media \
+    -- bash -c './unlink_media.sh && sleep 30s && ./link_media.sh'
+
+gnome-terminal \
+    --tab \
+    --working-directory /home/mmorano/git/mikeandwan.us/src/auth \
+    --title auth \
+    -- dotnet run;
+
+gnome-terminal \
+    --tab \
+    --working-directory /home/mmorano/git/mikeandwan.us/src/api \
+    --title api \
+    -- dotnet run;
+
+gnome-terminal \
+    --tab \
+    --working-directory /home/mmorano/git/mikeandwan.us/src/www \
+    --title www \
+    -- dotnet run

ed743efb314d62e9959efdb0cc84e36334a137da
Mike Morano <mmorano@mikeandwan.us>
2023-09-30 14:06:39 -0400

    try to simplify and optimize builds

--- _prod_build_and_push.sh
+++ _prod_build_and_push.sh
@@ -2,23 +2,22 @@
 IID_FILE=image.iid
 TAG=$(date +%Y%m%d%H%M%S)
 
-# volume mount is a workaround for a current COPY perf issue
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api --iidfile "${IID_FILE}"
+buildah bud -f Containerfile.api -t maw-api --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:latest
 
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth --iidfile "${IID_FILE}"
+buildah bud -f Containerfile.auth -t maw-auth --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:latest
 
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.www -t maw-www --iidfile "${IID_FILE}"
+buildah bud -f Containerfile.www -t maw-www --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:latest
 
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.cache -t maw-cache-sync --iidfile "${IID_FILE}"
+buildah bud -f Containerfile.cache -t maw-cache-sync --iidfile "${IID_FILE}"
 IID=$(cat "${IID_FILE}")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-cache-sync:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-cache-sync:latest
--- build-test-containers.sh
+++ build-test-containers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# volume mount is a workaround for a current COPY perf issue
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.api -t maw-api-test
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.auth -t maw-auth-test
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.www -t maw-www-test
-buildah bud -v "$(pwd)":/tmp/context:ro,Z -f Containerfile.cache -t maw-cache-sync-test
+buildah bud -f Containerfile.api   -t maw-api-test
+buildah bud -f Containerfile.auth  -t maw-auth-test
+buildah bud -f Containerfile.www   -t maw-www-test
+buildah bud -f Containerfile.cache -t maw-cache-sync-test

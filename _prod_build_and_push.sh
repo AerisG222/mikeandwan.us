0a24174325bf6fba982d300873c7f66bdc7f6202
Mike Morano <mmorano@mikeandwan.us>
2024-08-05 13:23:57 -0400

    try to specify unique iid files

--- _prod_build_and_push.sh
+++ _prod_build_and_push.sh
@@ -2,22 +2,22 @@
 IID_FILE=image.iid
 TAG=$(date +%Y%m%d%H%M%S)
 
-buildah bud -f Containerfile.api -t maw-api --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
+buildah bud -f Containerfile.api -t maw-api --iidfile "${IID_FILE}.api"
+IID=$(cat "${IID_FILE}.api")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-api:latest
 
-buildah bud -f Containerfile.auth -t maw-auth --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
+buildah bud -f Containerfile.auth -t maw-auth --iidfile "${IID_FILE}.auth"
+IID=$(cat "${IID_FILE}.auth")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-auth:latest
 
-buildah bud -f Containerfile.www -t maw-www --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
+buildah bud -f Containerfile.www -t maw-www --iidfile "${IID_FILE}.www"
+IID=$(cat "${IID_FILE}.www")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-www:latest
 
-buildah bud -f Containerfile.cache -t maw-cache-sync --iidfile "${IID_FILE}"
-IID=$(cat "${IID_FILE}")
+buildah bud -f Containerfile.cache -t maw-cache-sync --iidfile "${IID_FILE}.cache"
+IID=$(cat "${IID_FILE}.cache")
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-cache-sync:"${TAG}"
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-cache-sync:latest

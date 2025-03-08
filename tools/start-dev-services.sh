c70c16f204cf7ec5459a1f8cc7d3a2166a8f1117
Mike Morano <mmorano@mikeandwan.us>
2025-03-08 09:50:59 -0500

    update terminal app

--- tools/start-dev-services.sh
+++ tools/start-dev-services.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 systemctl --user start pod-dev-maw-pod.service
 
-gnome-terminal \
+ptyxis \
     --tab \
     --working-directory /home/mmorano/git/mikeandwan.us/tools \
     --title media \
     -- bash -c './unlink_media.sh && sleep 30s && ./link_media.sh'
 
-gnome-terminal \
+ptyxis \
     --tab \
     --working-directory /home/mmorano/git/mikeandwan.us/src/auth \
     --title auth \
     -- dotnet run;
 
-gnome-terminal \
+ptyxis \
     --tab \
     --working-directory /home/mmorano/git/mikeandwan.us/src/api \
     --title api \
     -- dotnet run;
 
-gnome-terminal \
+ptyxis \
     --tab \
     --working-directory /home/mmorano/git/mikeandwan.us/src/www \
     --title www \
